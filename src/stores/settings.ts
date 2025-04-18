10949df265e024faa2b8f0517a4b5b95bf4da763
Akirami <66513481+A-kirami@users.noreply.github.com>
2025-04-18 20:14:59 +0800

    ✨ 支持自定义角色默认动作 (#18)

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -5,12 +5,14 @@ export const useSettingsStore = defineStore('settings', () => {
   const dialogueAssociateFigure = $ref(false)
   const enableStrictScript = $ref(false)
   const removeTrailingPeriodInDialogue = $ref(false)
+  const figureDefaultAction = $ref('')
 
   return $$({
     theme,
     dialogueAssociateFigure,
     enableStrictScript,
     removeTrailingPeriodInDialogue,
+    figureDefaultAction,
   })
 }, {
   persist: true,
