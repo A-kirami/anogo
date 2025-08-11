8a347b1be7bd70f1a5f9009ddb11d3af1c34c266
Akirami <66513481+A-kirami@users.noreply.github.com>
2025-08-11 18:06:28 +0800

    👷 修复工件评论脚本的下载 URL 生成错误

--- scripts/formatArtifactComment.js
+++ scripts/formatArtifactComment.js
@@ -14,11 +14,12 @@ const platformIcon = {
  * @param {number} param0.id
  * @param {string} param0.name
  * @param {number} param0.size_in_bytes
+ * @param {{id:number}} param0.workflow_run
  */
-function createMarkdownTableRow({ id, name, size_in_bytes }) {
+function createMarkdownTableRow({ id, name, size_in_bytes, workflow_run }) {
   const platform = name.split('_')[2]
   const icon = platformIcon[platform.split('-')[0]] || platformIcon.unknown
-  const url = `https://github.com/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}/artifacts/${id}`
+  const url = `https://github.com/${process.env.GITHUB_REPOSITORY}/actions/runs/${workflow_run.id}/artifacts/${id}`
   const fileSize = Number.parseFloat((size_in_bytes / 1024 ** 2).toFixed(2))
   return `| ${icon} ${platform} | [${name}](${url}) | ${fileSize} MB |`
 }
@@ -28,14 +29,14 @@ function createMarkdownTableHeader() {
 }
 
 /**
- * @param {{id:number, name: string, size_in_bytes: number}[]} artifacts
+ * @param {{id:number, name: string, size_in_bytes: number, workflow_run: {id:number}}[]} artifacts
  * @param {string} sha
  */
 function createArtifactComment(artifacts, sha) {
   const tableHeader = createMarkdownTableHeader()
   const tableBody = artifacts
     .sort((a, b) => a.name.localeCompare(b.name))
-    .map(artifact => createMarkdownTableRow(artifact))
+    .map((artifact) => createMarkdownTableRow(artifact))
   const comment = [
     '### 📦️ 此 PR 构建的应用已经准备就绪',
     '',
